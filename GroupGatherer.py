26cb71d0d6036754090a185c37bc505602a90f48
smsmits <16short@gmail.com>
2020-06-17 16:33:00 -0700

    Finalizing

--- GroupGatherer.py
+++ GroupGatherer.py
@@ -39,7 +39,9 @@ options.add_argument("--num-raster-threads=1")
 options.add_argument("--disable-logging")
 options.add_argument("--incognito")
 options.add_argument("--log-level=3")
-driver = webdriver.Chrome(options=options)
+with open("chromedriver_path.txt", "r") as file:
+    path = file.read()
+driver = webdriver.Chrome(executable_path=path, options=options)
 
 # Variables
 line_break = "\n-------------------------------------------------------------------------------------------------------"
