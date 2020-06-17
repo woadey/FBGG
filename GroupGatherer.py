bc297a8c111197d8ece8f6e44f304360aa08328b
smsmits <16short@gmail.com>
2020-06-16 19:22:48 -0700

    CRAWLING FULLY FUNCTIONING

--- GroupGatherer.py
+++ GroupGatherer.py
@@ -12,6 +12,7 @@ from selenium.common.exceptions import TimeoutException
 options = Options()
 # options.add_argument("--headless")
 # options.add_argument("--blink-settings=imagesEnabled=false")
+# options.add_argument("--disable-threaded-compositing")
 options.add_argument("--disable-gpu")
 options.add_argument("--disable-notifications")
 options.add_argument("--disable-extensions")
@@ -33,13 +34,12 @@ options.add_argument("--disable-flash-stage3d")
 options.add_argument("--disable-gpu-vsync")
 options.add_argument("--disable-software-rasterizer")
 options.add_argument("--disable-sync")
-# options.add_argument("--disable-threaded-compositing")
 options.add_argument("--disable-threaded-scrolling")
 options.add_argument("--disable-webgl")
 options.add_argument("--disable-webgl-image-chromium")
 options.add_argument("--num-raster-threads=1")
 options.add_argument("--disable-logging")
-# options.add_argument("--incognito")
+options.add_argument("--incognito")
 options.add_argument("--window-size=1024,768")
 options.add_argument("--log-level=3")
 driver = webdriver.Chrome(options=options)
@@ -67,32 +67,32 @@ comment = {
 }
 
 
+class Tab:
+    MEMBERS = 1
+    DISCUSSION = 2
+
+
 def main():
-    try:
-        # Login
-        login()
+    # Login
+    login()
 
-        # Crawl About
-        crawl_about()
+    # Crawl About
+    crawl_about()
 
-        # Crawl Members
-        # member_page = crawl_members()
+    # Crawl Members
+    member_page = crawl_members()
 
-        # Crawl Content
-        discussion_page = crawl_discussion()
+    # Crawl Content
+    discussion_page = crawl_discussion()
 
-        # Scrape Members
-        # scrape_members(member_page)
+    # Close Driver
+    driver.close()
 
-        # Scrape Discussion
-        scrape_discussion(discussion_page)
-    except Exception as ex:
-        raise Exception(
-            "Failed scraping : " + str(ex)
-        )
-    finally:
-        # Close Driver
-        driver.close()
+    # Scrape Members
+    scrape_members(member_page)
+
+    # Scrape Discussion
+    scrape_discussion(discussion_page)
 
 
 def login():
@@ -152,38 +152,45 @@ def crawl_members():
         )
 
     print("Loading all members...")
-    scroll_to_element('div.mam> div > a[id][rel="async"]')
+    scroll(Tab.MEMBERS, 'div.mam> div > a[id][rel="async"]')
     print("Crawling all members...")
     print("COMPLETED" + line_break)
     return beautify_page()
 
 
 def scrape_members(page):
-    mem_container = page.select_one("#groupsMemberSection_all_members")
-    if mem_container is None:
-        mem_container = page.select_one("#groupsMemberSection_recently_joined")
+    # TODO test 'or' vs #
+    mem_container = page.select_one("#groupsMemberSection_all_members") or page.select_one(
+        "#groupsMemberSection_recently_joined")
+    # if mem_container is None:
+    #     mem_container = page.select_one("#groupsMemberSection_recently_joined")
+
     members_list = mem_container.select("div[class='clearfix _60rh _gse']")
     print("MEMBERS SCRAPED : " + str(len(members_list)))
     global member_count
-    print("SUCCESS RATE : " + str(int(len(members_list)/member_count*100)) + "%" + line_break)
+    print("SUCCESS RATE : " + str(int(len(members_list) / member_count * 100)) + "%" + line_break)
     for mem in members_list:
-        mem_name = mem.select_one("a[ajaxify]").get('title')
-        mem_url = mem.select_one("a[ajaxify]").get('href')
-        mem_type = mem.select_one("a[ajaxify*='badge_type=']")
-        if mem_type is None:
-            mem_type = "Member"
-        else:
-            mem_type = mem_type.text
-        member = {
-            'MEMBER_NAME': mem_name,
-            'MEMBER_URL': mem_url,
-            'MEMBER_TYPE': mem_type
-        }
-        global all_members
-        all_members.append(member)
+        try:
+            mem_name = mem.select_one("a[ajaxify]").get('title')
+            mem_url = mem.select_one("a[ajaxify]").get('href')
+            mem_type = mem.select_one("a[ajaxify*='badge_type=']")
+            if mem_type is None:
+                mem_type = "Member"
+            else:
+                mem_type = mem_type.text
+            member = {
+                'MEMBER_NAME': mem_name,
+                'MEMBER_URL': mem_url,
+                'MEMBER_TYPE': mem_type
+            }
+            global all_members
+            all_members.append(member)
+        except Exception as ex:
+            raise Exception(
+                "Error adding member : " + str(ex) + "\n\n\n\n\n" + mem.prettify()
+            )
 
 
-# TODO fix 'view all'
 def crawl_discussion():
     try:
         driver.find_element_by_link_text("Discussion").click()
@@ -193,18 +200,8 @@ def crawl_discussion():
         )
 
     print("Loading all group content...")
-    scroll_to_element('#pagelet_group_pager > div > div[id*="u_fetchstream_"]')
+    scroll(Tab.DISCUSSION, '#pagelet_group_pager > div > div > div > div[data-testid="fbfeed_placeholder_story"]')
     print("Scraping all group content...")
-
-    while True:
-        try:
-            view_more = driver.find_element_by_css_selector("a._4sxc._42ft")
-            driver.execute_script("arguments[0].scrollIntoView(true);", view_more)
-            view_more.click()
-            print("Viewed More")
-        except Exception as ex:
-            print("Completed Expanding : " + str(ex).strip())
-            break
     soup = beautify_page()
     check_posts = soup.select("div[data-testid='post_message']")
     print("POSTS SCRAPED : " + str(len(check_posts)) + line_break)
@@ -221,15 +218,71 @@ def beautify_page():
     return BeautifulSoup(driver.page_source, "lxml")
 
 
-def scroll_to_element(loading_selector):
+def scroll(tab, loading_selector):
     print("~~SCROLLING~~")
     time.sleep(3)
     while True:
         try:
+            if tab is Tab.DISCUSSION:
+                view_all()
+            wait_to_load(tab)
             driver.execute_script("window.scrollBy(0,document.body.scrollHeight)")
-            WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, loading_selector)))
+            if check_if_bottomed(loading_selector):
+                break
         except Exception as ex:
-            print("Done Scrolling : " + str(ex).strip())
+            print("Error when scrolling : " + str(ex).strip())
+            break
+
+
+def check_if_bottomed(loading_selector):
+    try:
+        driver.find_element_by_css_selector(loading_selector)
+        return False
+    except NoSuchElementException:
+        return True
+    except Exception as ex:
+        raise Exception(
+            "Error checking for bottom of page : " + str(ex)
+        )
+
+
+def wait_to_load(tab):
+    if tab is Tab.MEMBERS:
+        try:
+            WebDriverWait(driver, 10).until(EC.invisibility_of_element(
+                (By.CSS_SELECTOR, 'div.morePager > div > span > img')))
+        except TimeoutException:
+            pass
+        except Exception as ex:
+            raise Exception(
+                "Error waiting to load page : " + str(ex)
+            )
+    elif tab is Tab.DISCUSSION:
+        try:
+            WebDriverWait(driver, 10).until(EC.invisibility_of_element(
+                (By.CSS_SELECTOR, 'div.async_saving > div[data-testid="fbfeed_placeholder_story"]')))
+        except TimeoutException:
+            pass
+        except Exception as ex:
+            raise Exception(
+                "Error waiting to load page : " + str(ex)
+            )
+
+
+def view_all():
+    print("++EXPANDING COMMENTS++")
+    while True:
+        try:
+            wait_to_load(Tab.DISCUSSION)
+            view_more = driver.find_element_by_css_selector("a._4sxc._42ft")
+            driver.execute_script("arguments[0].scrollIntoView(false);", view_more)
+            driver.execute_script("window.scrollBy(0,250)")
+            view_more.click()
+        except NoSuchElementException:
+            print("Completed Expanding")
+            break
+        except Exception as ex:
+            print("Error Expanding Comments : " + str(ex).strip())
             break
 
 
